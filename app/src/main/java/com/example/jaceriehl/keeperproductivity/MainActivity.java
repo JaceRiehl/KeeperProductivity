d85feeb5a5f3c0dc2152ebd02a3626803058e71d
JaceRiehl <jace.riehl@gmail.com>
2018-03-25 16:04:10 -0600

    Added onclick listener and fixed build issue

--- app/src/main/java/com/example/jaceriehl/keeperproductivity/MainActivity.java
+++ app/src/main/java/com/example/jaceriehl/keeperproductivity/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.jaceriehl.keeperproductivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button newList = findViewById(R.id.newList);
+        newList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
