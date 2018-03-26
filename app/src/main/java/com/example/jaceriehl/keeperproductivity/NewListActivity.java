6bb853353bc6ffe68e27ae3393078a1e85ce54d3
JaceRiehl <jace.riehl@gmail.com>
2018-03-26 13:53:22 -0600

    Added some more attributes

--- app/src/main/java/com/example/jaceriehl/keeperproductivity/NewListActivity.java
+++ app/src/main/java/com/example/jaceriehl/keeperproductivity/NewListActivity.java
@@ -1,7 +1,12 @@
 package com.example.jaceriehl.keeperproductivity;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewListActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class NewListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_list);
+
+        final Button checkBox = findViewById(R.id.newListCheck);
+        final EditText enterBoardName = findViewById(R.id.enterBoardName);
+        enterBoardName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!enterBoardName.getText().equals(""))
+                    checkBox.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.checkBoxes));
+                else
+                    checkBox.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.ghostText));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
+
+
 }
