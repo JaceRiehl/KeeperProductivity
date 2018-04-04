8cdc9070a3fe3164842edd20fc828a9433a37d97
JaceRiehl <jace.riehl@gmail.com>
2018-04-04 13:50:15 -0600

    Added back button to action bar

--- app/src/main/java/com/example/jaceriehl/keeperproductivity/TasksActivity.java
+++ app/src/main/java/com/example/jaceriehl/keeperproductivity/TasksActivity.java
@@ -1,21 +1,37 @@
 package com.example.jaceriehl.keeperproductivity;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class TasksActivity extends AppCompatActivity {
-
+    protected Intent returnIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
-
-       // getActionBar().setHomeButtonEnabled(true);
-        //getActionBar().setDisplayHomeAsUpEnabled(true);
-        //getActionBar().setTitle("Title");
+        ActionBar action = getSupportActionBar();
+        action.setHomeButtonEnabled(true);
+        action.setDisplayHomeAsUpEnabled(true);
+        action.setTitle("Title");
         //getSupportActionBar().setTitle("Hello world App");
-        Intent returnIntent = getIntent();
+        returnIntent = getIntent();
 
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                returnIntent = getIntent();
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
