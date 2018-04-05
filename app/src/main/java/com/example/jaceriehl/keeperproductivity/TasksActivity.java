3b59df9abdf8fdb0cfedc0a6fa03d8e9845b891e
JaceRiehl <jace.riehl@gmail.com>
2018-04-05 17:42:45 -0600

    Added an add tasks button to the tasks activity

--- app/src/main/java/com/example/jaceriehl/keeperproductivity/TasksActivity.java
+++ app/src/main/java/com/example/jaceriehl/keeperproductivity/TasksActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import java.util.List;
@@ -14,9 +16,11 @@ public class TasksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
+
         ActionBar action = getSupportActionBar();
         action.setHomeButtonEnabled(true);
         action.setDisplayHomeAsUpEnabled(true);
+
         //action.setTitle("Title");
         //getSupportActionBar().setTitle("Hello world App");
         returnIntent = getIntent();
@@ -30,12 +34,22 @@ public class TasksActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_tasks_activity,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         switch(item.getItemId()) {
             case android.R.id.home:
                 returnIntent = getIntent();
                 finish();
+                return true;
+            case R.id.action_add:
+
                 return true;
         }
         return super.onOptionsItemSelected(item);
