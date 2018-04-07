205e2416870087f846b9b1857fff9b4122b3de4b
JaceRiehl <jace.riehl@gmail.com>
2018-04-06 22:51:34 -0600

    Added delete to lists and added spinners to newTaskActivity

--- app/src/main/java/com/example/jaceriehl/keeperproductivity/ListsAdapter.java
+++ app/src/main/java/com/example/jaceriehl/keeperproductivity/ListsAdapter.java
@@ -12,6 +12,7 @@ import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -34,12 +35,12 @@ public class ListsAdapter extends RecyclerView.Adapter<ListsAdapter.ViewHolder>{
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
         public TextView mTextView;
-        public ImageButton mRemoveButton;
+        public Button mRemoveButton;
         public LinearLayout mRelativeLayout;
         public ViewHolder(View v){
             super(v);
             mTextView = (TextView) v.findViewById(R.id.tv);
-//            mRemoveButton = (ImageButton) v.findViewById(R.id.ib_remove);
+            mRemoveButton = (Button) v.findViewById(R.id.removeList);
             mRelativeLayout = (LinearLayout) v.findViewById(R.id.rl);
         }
     }
@@ -115,50 +116,50 @@ public class ListsAdapter extends RecyclerView.Adapter<ListsAdapter.ViewHolder>{
 
 
         // Set a click listener for item remove button
-//        holder.mRemoveButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                // Get the clicked item label
-//                String itemLabel = mDataSet.get(position);
-//
-//                // Remove the item on remove/button click
-//                mDataSet.remove(position);
-//
-//                /*
-//                    public final void notifyItemRemoved (int position)
-//                        Notify any registered observers that the item previously located at position
-//                        has been removed from the data set. The items previously located at and
-//                        after position may now be found at oldPosition - 1.
-//
-//                        This is a structural change event. Representations of other existing items
-//                        in the data set are still considered up to date and will not be rebound,
-//                        though their positions may be altered.
-//
-//                    Parameters
-//                        position : Position of the item that has now been removed
-//                */
-//                notifyItemRemoved(position);
-//
-//                /*
-//                    public final void notifyItemRangeChanged (int positionStart, int itemCount)
-//                        Notify any registered observers that the itemCount items starting at
-//                        position positionStart have changed. Equivalent to calling
-//                        notifyItemRangeChanged(position, itemCount, null);.
-//
-//                        This is an item change event, not a structural change event. It indicates
-//                        that any reflection of the data in the given position range is out of date
-//                        and should be updated. The items in the given range retain the same identity.
-//
-//                    Parameters
-//                        positionStart : Position of the first item that has changed
-//                        itemCount : Number of items that have changed
-//                */
-//                notifyItemRangeChanged(position,mDataSet.size());
-//
-//                // Show the removed item label
-//                Toast.makeText(mContext,"Removed : " + itemLabel,Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        holder.mRemoveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get the clicked item label
+                String itemLabel = mDataSet.get(position).getName();
+
+                // Remove the item on remove/button click
+                mDataSet.remove(position);
+
+                /*
+                    public final void notifyItemRemoved (int position)
+                        Notify any registered observers that the item previously located at position
+                        has been removed from the data set. The items previously located at and
+                        after position may now be found at oldPosition - 1.
+
+                        This is a structural change event. Representations of other existing items
+                        in the data set are still considered up to date and will not be rebound,
+                        though their positions may be altered.
+
+                    Parameters
+                        position : Position of the item that has now been removed
+                */
+                notifyItemRemoved(position);
+
+                /*
+                    public final void notifyItemRangeChanged (int positionStart, int itemCount)
+                        Notify any registered observers that the itemCount items starting at
+                        position positionStart have changed. Equivalent to calling
+                        notifyItemRangeChanged(position, itemCount, null);.
+
+                        This is an item change event, not a structural change event. It indicates
+                        that any reflection of the data in the given position range is out of date
+                        and should be updated. The items in the given range retain the same identity.
+
+                    Parameters
+                        positionStart : Position of the first item that has changed
+                        itemCount : Number of items that have changed
+                */
+                notifyItemRangeChanged(position,mDataSet.size());
+
+                // Show the removed item label
+                Toast.makeText(mContext,"Removed : " + itemLabel,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
